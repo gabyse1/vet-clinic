bc68c5a472993a2af71429be8c01759ae7318667
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-05 16:13:53 -0500

    Add invoice_items and medicalhistories_treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE clinic;
 
 CREATE TABLE patients (
-    id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(50),
     date_of_birth DATE
 );
 
 CREATE TABLE medical_histories (
-    id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     admitted_at TIMESTAMP,
     patient_id INTEGER,
     status VARCHAR(50),
@@ -15,17 +15,34 @@ CREATE TABLE medical_histories (
 );
 
 CREATE TABLE treatments (
-    id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR(50),
     name VARCHAR(50)
 );
 
+CREATE TABLE medicalhistories_treatments (
+    medical_history_id    INTEGER,
+    treatment_id    INTEGER,
+    CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
 CREATE TABLE invoices (
-    id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     total_amount DECIMAL,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
-    medical_history_id INTEGER
+    medical_history_id INTEGER,
+    CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
-
+CREATE TABLE invoice_items (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    unit_price  DECIMAL,
+    quantity    INTEGER,
+    total_price DECIMAL,
+    invoice_id  INTEGER,
+    treatment_id    INTEGER,
+    CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
